0b25be18cb0e6d4ac4f68a49477b5530efeee5d5
sakith <sakith@sakith-Aspire-5750>
2014-10-01 10:56:15 +0530

    test

--- org.eclipse.ecf.remoteservices.tooling.pde.templates/templates/TimeServiceHostTemplate/java/$Activator$.java
+++ org.eclipse.ecf.remoteservices.tooling.pde.templates/templates/TimeServiceHostTemplate/java/$Activator$.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 X2CODE, Inc. All rights reserved. This
+ * Copyright (c) 2013 Sakith Indula. All rights reserved. This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
